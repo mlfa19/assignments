7205f04c2b7e91fbb9f6fb8d2992dbc1ead00714
Paul Ruvolo <paullundyruvolo@gmail.com>
2019-08-27 00:26:03 -0400

    added support for generating solution set for the ipynb notebook and the non solution version

--- scripts/convert_ipynb.sh
+++ scripts/convert_ipynb.sh
@@ -5,6 +5,8 @@ for f in $FILES
 do
   echo "Processing $f file..."
   base=${f%.*}
-  jupyter nbconvert --execute --to html "$f" 
+  jupyter nbconvert --execute --to html --output ${base}_Solutions "$f" 
+  wkhtmltopdf "${base}_Solutions.html" "${base}_Solutions.pdf"
+  jupyter nbconvert --execute --TagRemovePreprocessor.enabled=True  Assignment_01_Companion.ipynb --TagRemovePreprocessor.remove_cell_tags="['solution']" --to html "$f" 
   wkhtmltopdf "${base}.html" "${base}.pdf"
 done
