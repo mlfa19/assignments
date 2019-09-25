3c088a5c8dd6c5f9d7277f6df8ab8ca4fc230cb8
Paul Ruvolo <paullundyruvolo@gmail.com>
2019-09-24 22:57:20 -0400

    added option to not execute solutions notebook

--- scripts/convert_ipynb.sh
+++ scripts/convert_ipynb.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+usage() { echo "Usage: $0 [-x]" 1>&2; exit 1; }
+
+execute_flag=""
+while getopts ":x" o; do
+    case "${o}" in
+        x)
+            execute_flag="--execute"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
 FILES=*.ipynb
 for f in $FILES
 do
   echo "Processing $f file..."
   base=${f%.*}
-  jupyter nbconvert --execute --RegexRemovePreprocessor.patterns="['.*Expand for Solution.*']" --to html --output ${base}_Solutions "$f" 
+  jupyter nbconvert ${execute_flag} --RegexRemovePreprocessor.patterns="['.*Expand for Solution.*']" --to html --output ${base}_Solutions "$f" 
   wkhtmltopdf "${base}_Solutions.html" "${base}_Solutions.pdf"
   jupyter nbconvert  --RegexRemovePreprocessor.patterns="['.*Expand for Solution.*', '.*\*\*\*Solution\*\*\*.*']" --to html "$f" 
   wkhtmltopdf "${base}.html" "${base}.pdf"
